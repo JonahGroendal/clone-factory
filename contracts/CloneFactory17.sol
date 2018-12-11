304eb3877183d30eefe3dea2f386dbb495a110a0
JonahGroendal <jone951@gmail.com>
2018-12-10 22:35:44 -0500

    Upgrade compiler to v<0.6.0

--- contracts/CloneFactory17.sol
+++ contracts/CloneFactory17.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity >=0.4.23  <0.6.0;
 
 /*
 The MIT License (MIT)
@@ -52,7 +52,7 @@ contract CloneFactory17 {
       extcodecopy(query, other, 0, 0x2a)
 
       result := and(
-        eq(mload(clone), mload(other)), 
+        eq(mload(clone), mload(other)),
         eq(mload(add(clone, 0x20)), mload(add(other, 0x20)))
       )
     }
